94bdf030b6b7dc802d195f2f520023e01ff44bd4
Ulrich Mayring <ulim@mayring.de>
2017-04-04 19:11:27 +0200

    distribute colors via Hazelcast

--- src/main/java/de/mayring/payarahazelcastexample/UuidService.java
+++ src/main/java/de/mayring/payarahazelcastexample/UuidService.java
@@ -1,6 +1,10 @@
 package de.mayring.payarahazelcastexample;
 
 import java.util.UUID;
+import java.util.concurrent.ThreadLocalRandom;
+
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
 import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
@@ -11,8 +15,13 @@ import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.UriInfo;
 
+import com.hazelcast.core.HazelcastInstance;
+import com.hazelcast.core.IList;
+
 @Path("uuid")
 public class UuidService {
+
+    private final HazelcastInstance hazelcast;
 
     @Context UriInfo uriInfo;
 
@@ -22,14 +31,16 @@ public class UuidService {
     private final String outputOnPost = "<html><h2 style='color:%1$s; font-family: sans-serif'>%2$s stored.</h2>" +
             "<a href='%3$s'>continue</a></html>";
 
-    public UuidService() {
+    public UuidService() throws NamingException {
+        javax.naming.Context ctx = new InitialContext();
+        hazelcast = (HazelcastInstance) ctx.lookup("payara/Hazelcast");
     }
 
     @GET
     @Produces(MediaType.TEXT_HTML)
     public String getRandom() {
         String uuid = UUID.randomUUID().toString();
-        String color = ApplicationConfig.getColor();
+        String color = getRandomColor();
         return String.format(outputOnGet, color, uuid, uriInfo.getAbsolutePath().toString() + "/" + uuid);
     }
 
@@ -40,6 +51,11 @@ public class UuidService {
         String uri = uriInfo.getAbsolutePath().toString();
         int end = uri.length() - (uuidToStore.length() + 1);
         return String.format(outputOnPost, color, uuidToStore, uri.substring(0, end));
+    }
+
+    private String getRandomColor() {
+        IList<String> colors = hazelcast.getList("colors");
+        return colors.get(ThreadLocalRandom.current().nextInt(0, colors.size()));
     }
 
 }
