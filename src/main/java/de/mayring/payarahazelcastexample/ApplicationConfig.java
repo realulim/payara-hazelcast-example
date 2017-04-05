8c0d1bd0ff3db6addd9d11822dc4599e61249fbf
Ulrich Mayring <ulim@mayring.de>
2017-04-05 09:16:04 +0200

    configuration of Health Checks removed

--- src/main/java/de/mayring/payarahazelcastexample/ApplicationConfig.java
+++ src/main/java/de/mayring/payarahazelcastexample/ApplicationConfig.java
@@ -2,13 +2,11 @@ package de.mayring.payarahazelcastexample;
 
 import java.util.Set;
 
+import javax.annotation.PostConstruct;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
-
-import fish.payara.micro.PayaraMicro;
-import fish.payara.micro.PayaraMicroRuntime;
 
 import com.hazelcast.core.HazelcastInstance;
 import com.hazelcast.core.IList;
@@ -29,18 +27,9 @@ public class ApplicationConfig extends Application {
         colors.add("Black");
     }
 
-    @javax.annotation.PostConstruct
+    @PostConstruct
     public void configurePayara() {
-        final PayaraMicroRuntime pmRuntime = PayaraMicro.getInstance().getRuntime();
-        pmRuntime.run("healthcheck-configure", "--enabled=true", "--dynamic=true");
-        pmRuntime.run("healthcheck-configure-service", "--serviceName=healthcheck-cpu", "--enabled=true",
-            "--time=5", "--unit=SECONDS", "--dynamic=true");
-        pmRuntime.run("healthcheck-configure-service-threshold", "--serviceName=healthcheck-cpu",
-            "--thresholdCritical=90", "--thresholdWarning=50", "--thresholdGood=0", "--dynamic=true");
-        pmRuntime.run("healthcheck-configure-service", "--serviceName=healthcheck-machinemem",
-            "--enabled=true", "--dynamic=true", "--time=5","--unit=SECONDS");
-        pmRuntime.run("healthcheck-configure-service-threshold", "--serviceName=healthcheck-machinemem",
-            "--thresholdCritical=90", "--thresholdWarning=50", "--thresholdGood=0", "--dynamic=true");
+        // not working in Payara 4.1.1.171.1
     }
 
     @Override
